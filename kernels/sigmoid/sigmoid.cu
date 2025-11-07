94bdae05c0f10207ce61ce32eb7abb177107660e
violet <zjz0@outlook.com>
2025-11-07 07:00:35 +0000

    update

--- kernels/sigmoid/sigmoid.cu
+++ kernels/sigmoid/sigmoid.cu
@@ -16,7 +16,7 @@
 #define HALF2(value) (reinterpret_cast<half2 *>(&(value))[0])
 #define BFLOAT2(value) (reinterpret_cast<__nv_bfloat162 *>(&(value))[0])
 #define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
-#define MAX_EXP_F32 88.3762626647949f
+#define MAX_EXP_F32 88.3762626647949f // exp(89) ≈ 4.5e38,接近 float 最大值
 #define MIN_EXP_F32 -88.3762626647949f
 #define MAX_EXP_F16 __float2half(11.089866488461016f)
 #define MIN_EXP_F16 __float2half(-9.704060527839234f)
